348709c21fd749cf42afcd0c3ec29e23fa35283a
Vincent Ho <vinmaster99@gmail.com>
2020-12-06 13:45:22 -0800

    Add 2019 day 11 js

--- 2019/day11/day11.js
+++ 2019/day11/day11.js
@@ -9,6 +9,7 @@ class Program {
     this.pointer = 0;
     this.increment = 0;
     this.relativeBase = 0;
+    this.exit = false;
   }
 
   getMemory(program, address) {
@@ -34,11 +35,9 @@ class Program {
   }
 
   run(inputs) {
-    this.outputs = [];
-    this.inputs = this.inputs.concat(inputs);
-    for (;; this.pointer += this.increment) {
+    this.inputs = inputs;
+    for (; !this.exit; this.pointer += this.increment) {
       const opcodeData = this.program[this.pointer];
-      if (opcodeData === 99) { throw new Error('99'); }
 
       const input1 = this.getMemory(this.program, this.pointer + 1);
       const input2 = this.getMemory(this.program, this.pointer + 2);
@@ -84,16 +83,16 @@ class Program {
           this.increment = 4;
           break;
         case 3:
-          this.editMemory(this.program, input1, this.inputs.shift(), param1Mode, this.relativeBase);
+          let input = this.inputs.shift();
+          this.editMemory(this.program, input1, input, param1Mode, this.relativeBase);
+          this.inputs.push(input);
           this.increment = 2;
           break;
         case 4:
-          // console.log('this.program output', param1);
           this.outputs.push(param1);
-          if (this.outputs.length === 2) {
-            return this.outputs;
-          }
           this.increment = 2;
+          this.pointer += this.increment;
+          return this.outputs;
           break;
         case 5:
           if (param1 !== 0) {
@@ -126,47 +125,69 @@ class Program {
           this.increment = 2;
           break;
         case 99:
+          this.exit = true;
           break;
         default:
           throw new Error(`Opcode invalid: ${opcode}, ${opcodeData}`);
       }
     }
-    return this.outputs;
   }
 }
 
 class Robot {
-  constructor({ x, y, dir, intcode, grid }) {
+  constructor({ x, y, dir, intcode, startingPanel = '.' }) {
     this.x = x || 0;
     this.y = y || 0;
     this.dir = dir || 0;
-    this.grid = grid;
+    this.grid = {};
+    this.grid[`${this.x},${this.y}`] = startingPanel;
     this.program = new Program(intcode);
     this.painted = new Map();
   }
 
+  get position() {
+    return `${this.x},${this.y}`;
+  }
+
+  get currentPanel() {
+    return this.grid[this.position];
+  }
+
+  get direction() {
+    if (this.dir === 0) {
+      return 'Up';
+    } else if (this.dir === 1) {
+      return 'Left';
+    } else if (this.dir === 2) {
+      return 'Down';
+    } else if (this.dir === 3) {
+      return 'Right';
+    }
+  }
+
   paint(paint) {
     if (paint === 0) {
-      this.grid[this.y][this.x] = '.';
+      this.grid[this.position] = '.';
     } else if (paint === 1) {
-      this.grid[this.y][this.x] = '#';
+      this.grid[this.position] = '#';
     } else {
       throw new Error('Invalid paint');
     }
 
-    const key = `${this.y},${this.x}`;
-    let value = this.painted.get(key);
+    let value = this.painted.get(this.position);
     if (!value) {
       value = 0;
     }
     value += 1;
-    this.painted.set(key, value);
+    this.painted.set(this.position, value);
   }
 
   turn(turn) {
-    if (turn === 0) {
+    let LEFT = 0;
+    let RIGHT = 1;
+    if (turn === LEFT) {
       this.dir += 1;
-    } else if (turn === 1) {
+    } else if (turn === RIGHT) {
       this.dir -= 1;
     } else {
       throw new Error('Invalid turn');
@@ -191,51 +212,42 @@ class Robot {
   }
 
   run() {
-    try {
-      // const p = new Program(program, [grid[this.y][this.x]]);1
-      for (let i = 0; i < 15199; i++) {
-        const input = this.grid[this.y][this.x] === '.' ? 0 : 1;
-        let [paint, turn] = this.program.run([input]);
-        // console.log('output', paint, turn, input);
+    for (let i = 0; !this.program.exit; i++) {
+      const input = this.currentPanel === '#' ? 1 : 0;
+      this.program.run([input]);
+      if (this.program.outputs.length > 1) {
+        let [paint, turn] = [this.program.outputs.shift(), this.program.outputs.shift()];
+
         this.paint(paint);
         this.turn(turn);
         this.move();
       }
-    } catch (error) {
-      if (error.message !== '99') {
-        console.log(error);
+    }
+  }
+
+  getGridStr() {
+    let coords = Object.keys(this.grid).map(c => c.split(',').map(v => parseInt(v, 10)));
+    let xs = coords.map(c => c[0]);
+    let ys = coords.map(c => c[1]);
+
+    xs.sort((a, b) => a - b);
+    ys.sort((a, b) => a - b);
+
+    let min_x = xs[0];
+    let max_x = xs[xs.length - 1];
+    let min_y = ys[0];
+    let max_y = ys[ys.length - 1];
+
+    let str = '\n';
+    for (let y = min_y; y <= max_y; y++) {
+      for (let x = min_x; x <= max_x; x++) {
+        let cell = `${x},${y}`;
+        str += this.grid[cell] === '#' ? '#' : ' ';
       }
-      console.log('------------------done');
+      str += '\n';
     }
-    
-    let robot = '!';
-    // console.log('dir', this.dir);
-    if (this.dir === 0) {
-      // Up
-      robot = '^';
-    } else if (this.dir === 1) {
-      // Left
-      robot = '<';
-    } else if (this.dir === 2) {
-      // Down
-      robot = 'v';
-    } else if (this.dir === 3) {
-      // Right
-      robot = '>';
-    }
-    if (this.y < 0 ||
-      this.y > this.grid.length ||
-      this.x < 0 ||
-      this.x > this.grid[0].length
-    ) {
-      throw new Error(`Out of range ${this.x} ${this.y} of ${this.grid.length} ${this.grid[0].length}`);
-    }
-    this.grid[this.y][this.x] = robot;
-    for (const row of this.grid) {
-      // console.log(row.join(''));
-    }
-    // console.log([...this.painted]);
-    console.log(this.painted.size);
+
+    return str;
   }
 }
 
@@ -245,31 +257,402 @@ function part1(input) {
     .split(',')
     .map(s => parseInt(s, 10));
 
-  const rows = 5000;
-  const cols = 5000;
-  const grid = Array.from(Array(rows), _ => Array(cols).fill('.'));
-  console.log('range', grid.length, grid[0].length);
   const robot = new Robot({
-    x: 1000, y: 1000, grid, intcode,
+    x: 0, y: 0, intcode,
   });
   robot.run();
-  // const painted = new Map();
-  // const robot = {
-  //   x: 25, y: 25, dir: 0, // 0 - up, 1 - left, 2 - down, 3 - right
-  // };
-  
-  return;
+
+  return robot.painted.size;
 }
 
 function part2(input) {
-  const program = input
+  const intcode = input
     .trim()
     .split(',')
     .map(s => parseInt(s, 10));
 
-  return runProgram(program, 2);
+  const robot = new Robot({
+    x: 0, y: 0, intcode, startingPanel: '#',
+  });
+  robot.run();
+
+  return robot.getGridStr();
 }
 
 const input = fs.readFileSync(path.resolve(__dirname, './input.txt'), 'utf8');
 console.log('day11 part1:', part1(input));
-// console.log('day11 part2:', part2(input));
+console.log('day11 part2:', part2(input));
+
+/*
+// Part 1
+const { input } = require('./input');
+const { Ship } = require('./intcode-computer');
+
+let ship = new Ship(input);
+let output = ship.run();
+
+console.log(output);
+// Part 2
+const { input } = require('./input');
+const { Ship } = require('./intcode-computer');
+
+let ship = new Ship(input, 1);
+ship.run();
+ship.printShip();
+
+// intcode-computer.js
+const ADD = '01'; // Add
+const MUL = '02'; // Multiply
+const INP = '03'; // Input
+const OUT = '04'; // Output
+const JIT = '05'; // Jump-if-true
+const JIF = '06'; // Jump-if-false
+const LTH = '07'; // Less Than
+const EQU = '08'; // Equals
+const ARB = '09'; // Adjust relative base
+const STP = '99'; // Stop
+
+const POSITION_MODE = '0';
+const IMMEDIATE_MODE = '1';
+const RELATIVE_MODE = '2';
+
+class Computer {
+  constructor(
+    memory,
+    inputs,
+    replenish_input,
+    pause_on_output = true,
+    id = 0,
+    clone_memory = false
+  ) {
+    // For debugging
+    this.id = String.fromCharCode('A'.charCodeAt(0) + id);
+
+    this.original_memory = clone_memory && memory.slice(0);
+    this.memory = memory.slice(0);
+    this.pointer = 0;
+    this.relative_base = 0;
+    this.pause_on_output = pause_on_output;
+
+    this.inputs = Array.isArray(inputs) ? inputs.slice(0) : [inputs];
+    this.replenish_input = replenish_input;
+    this.outputs = [];
+
+    this.OPS = {
+      [ADD]: {
+        name: ADD,
+        realName: 'ADD',
+        params: 3,
+        fn: (a, b, c) => {
+          this.memory[c] = a + b;
+        },
+        write: true,
+      },
+
+      [MUL]: {
+        name: MUL,
+        realName: 'MUL',
+        params: 3,
+        fn: (a, b, c) => {
+          this.memory[c] = a * b;
+        },
+        write: true,
+      },
+
+      [INP]: {
+        name: INP,
+        realName: 'INP',
+        params: 1,
+        fn: a => {
+          this.memory[a] = this.inputs.shift();
+          if (this.replenish_input !== undefined) {
+            this.inputs.push(this.replenish_input);
+          }
+        },
+        write: true,
+      },
+
+      [OUT]: {
+        name: OUT,
+        realName: 'OUT',
+        params: 1,
+        fn: a => this.output(a),
+      },
+
+      [ARB]: {
+        name: ARB,
+        realName: 'ARB',
+        params: 1,
+        fn: a => (this.relative_base += a),
+      },
+
+      [STP]: {
+        name: STP,
+        realName: 'STP',
+        params: 0,
+        fn: () => (this.halted = true),
+      },
+
+      [JIT]: {
+        name: JIT,
+        realName: 'JIT',
+        params: 2,
+        fn: (a, b) => {
+          if (a) {
+            this.pointer = b;
+            return true;
+          }
+          return false;
+        },
+        jumps: true,
+      },
+
+      [JIF]: {
+        name: JIF,
+        realName: 'JIF',
+        params: 2,
+        fn: (a, b) => {
+          if (!a) {
+            this.pointer = b;
+            return true;
+          }
+          return false;
+        },
+        jumps: true,
+      },
+
+      [LTH]: {
+        name: LTH,
+        realName: 'LTH',
+        params: 3,
+        fn: (a, b, c) => {
+          this.memory[c] = a < b ? 1 : 0;
+        },
+        write: true,
+      },
+
+      [EQU]: {
+        name: EQU,
+        realName: 'EQU',
+        params: 3,
+        fn: (a, b, c) => {
+          this.memory[c] = a === b ? 1 : 0;
+        },
+        write: true,
+      },
+    };
+
+    this.halted = false;
+  }
+
+  run() {
+    let op = this.parseOp();
+
+    while (!this.halted) {
+      this.runOp(op);
+
+      if ((this.pause_on_output && op.name === OUT) || this.halted) {
+        break;
+      }
+
+      op = this.parseOp();
+    }
+
+    return this.outputs;
+  }
+
+  parseOp() {
+    let temp_op = String(this.memory[this.pointer]).padStart(2, '0');
+
+    // "The opcode is a two-digit number based only on the ones and tens digit of the value, that is, the opcode is the rightmost two digits of the first value in an instruction"
+    let op = this.OPS[temp_op.substr(-2, 2)];
+
+    let full_op = temp_op.padStart(op.params + 2, '0');
+
+    let modes = [];
+
+    for (let i = op.params - 1; i >= 0; i--) {
+      modes.push(full_op[i]);
+    }
+
+    return {
+      ...op,
+      modes,
+    };
+  }
+
+  runOp({ modes, fn, jumps, write }) {
+    this.pointer++;
+    let values = [];
+    for (let i = 0; i < modes.length; i++) {
+      let mode = modes[i];
+      let value = this.memory[this.pointer + i];
+
+      // Values can overflow existing memory. If so, value should be 0
+      if (value === undefined) {
+        value = 0;
+      }
+
+      // Immediate Mode uses the value as is, no need to make adjustments
+      if (mode !== IMMEDIATE_MODE) {
+        const can_switch_to_position = !write || i < modes.length - 1;
+
+        if (can_switch_to_position && mode === POSITION_MODE) {
+          value = this.memory[value];
+        } else if (mode === RELATIVE_MODE) {
+          if (can_switch_to_position) {
+            value = this.memory[value + this.relative_base];
+          } else {
+            value = value + this.relative_base;
+          }
+        }
+      }
+
+      // After remapping (if any) again adjust to 0 if we overflowed our memory read
+      if (value === undefined) {
+        value = 0;
+      }
+
+      values.push(value);
+    }
+
+    // If result is `true`, we moved the pointer
+    let result = fn(...values);
+
+    if (!jumps || (jumps && !result)) {
+      this.pointer += modes.length;
+    }
+  }
+
+  output(v) {
+    this.outputs.push(v);
+  }
+
+  // For debugging
+  get _() {
+    return this.memory.slice(Math.max(0, this.pointer - 1), this.pointer + 8);
+  }
+}
+
+class Direction {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.direction = [0, -1];
+  }
+
+  rotateLeft() {
+    const [x, y] = this.direction;
+    if (x === 0 && y === -1) {
+      this.direction = [-1, 0];
+    } else if (x === -1 && y === 0) {
+      this.direction = [0, 1];
+    } else if (x === 0 && y === 1) {
+      this.direction = [1, 0];
+    } else if (x === 1 && y === 0) {
+      this.direction = [0, -1];
+    }
+
+    this.move();
+  }
+
+  rotateRight() {
+    const [x, y] = this.direction;
+    if (x === 0 && y === -1) {
+      this.direction = [1, 0];
+    } else if (x === 1 && y === 0) {
+      this.direction = [0, 1];
+    } else if (x === 0 && y === 1) {
+      this.direction = [-1, 0];
+    } else if (x === -1 && y === 0) {
+      this.direction = [0, -1];
+    }
+
+    this.move();
+  }
+
+  move() {
+    const [x, y] = this.direction;
+    this.x += x;
+    this.y += y;
+  }
+
+  get coord() {
+    return `${this.x},${this.y}`;
+  }
+}
+
+class Ship {
+  constructor(memory, starting_color = 0) {
+    this.computer = new Computer(memory, [starting_color], starting_color);
+
+    this.ship = { '0,0': starting_color };
+    this.direction = new Direction(0, 0);
+  }
+
+  run() {
+    let computer = this.computer;
+    let output;
+
+    while (!computer.halted) {
+      output = computer.run();
+      if (computer.halted) {
+        break;
+      }
+
+      if (output.length > 1) {
+        // 0 is black, 1 is white
+        let color = output.shift();
+        // 0 is turn left, 1 is turn right
+        let direction = output.shift();
+
+        this.ship[this.direction.coord] = color;
+
+        if (direction === 0) {
+          this.direction.rotateLeft();
+        } else if (direction === 1) {
+          this.direction.rotateRight();
+        } else {
+          console.log('ERRROR', direction);
+        }
+
+        // Set input replenish after move
+        let new_color = this.ship[this.direction.coord] || 0;
+        computer.inputs = [new_color];
+        computer.replenish_input = new_color;
+      }
+    }
+
+    return Object.keys(this.ship).length;
+  }
+
+  printShip() {
+    let coords = Object.keys(this.ship).map(c => c.split(',').map(v => parseInt(v, 10)));
+    let xs = coords.map(c => c[0]);
+    let ys = coords.map(c => c[1]);
+
+    xs.sort((a, b) => a - b);
+    ys.sort((a, b) => a - b);
+
+    let min_x = xs[0];
+    let max_x = xs[xs.length - 1];
+    let min_y = ys[0];
+    let max_y = ys[ys.length - 1];
+
+    let str = '';
+    for (let y = min_y; y <= max_y; y++) {
+      for (let x = min_x; x <= max_x; x++) {
+        let cell = `${x},${y}`;
+        str += this.ship[cell] ? '#' : ' ';
+      }
+      str += '\n';
+    }
+    console.log(str);
+  }
+}
+
+module.exports = {
+  Computer,
+  Ship,
+};
+*/
