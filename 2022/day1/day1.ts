3c3af922bb773b8b5e9fc063ef9b52fc172c53a8
Vincent Ho <vinmaster99@gmail.com>
2023-01-11 09:07:48 -0800

    try curry/pipe

--- 2022/day1/day1.ts
+++ 2022/day1/day1.ts
@@ -1,34 +1,32 @@
-// deno-lint-ignore-file prefer-const
+// deno-lint-ignore-file prefer-const no-explicit-any
 
 function part1(input: string) {
-  let lines = input.trim().split('\n\n');
-
-  let everyone = [];
-  for (let person of lines) {
-    let calories = person
-      .split('\n')
-      .map(n => parseInt(n, 10))
-      .reduce((a, b) => a + b);
-    everyone.push(calories);
-  }
-  let max = Math.max(...everyone);
-
-  return max;
+  return pipe(
+    trim,
+    curry(split)('\n\n'),
+    (list: string[]) => {
+      return list.map(s =>
+        s
+          .split('\n')
+          .map(Number)
+          // @ts-ignore use sum
+          .sum()
+      );
+    },
+    (list: number[]) => Math.max(...list)
+  )(input);
 }
 
 function part2(input: string) {
   let lines = input.trim().split('\n\n');
 
-  let everyone = [];
-  for (let person of lines) {
-    let calories = person
-      .split('\n')
-      .map(n => parseInt(n, 10))
-      .reduce((a, b) => a + b);
-    everyone.push(calories);
-  }
-  everyone.sort((a, b) => b - a);
-  let max = everyone.slice(0, 3).reduce((a, b) => a + b);
+  let max = lines
+    // @ts-ignore use sum
+    .map(person => person.split('\n').map(Number).sum())
+    // @ts-ignore use sortf
+    .sortf((a, b) => b - a)
+    .slice(0, 3)
+    .sum();
 
   return max;
 }
@@ -51,5 +49,40 @@ let input = await Deno.readTextFile(`${new URL('.', import.meta.url).pathname}in
 
 // 10000`;
 
+Object.defineProperty(Array.prototype, 'sortf', {
+  value: function (compareFn: (a: any, b: any) => any) {
+    return [].concat(this).sort(compareFn);
+  },
+});
+
+Object.defineProperty(Array.prototype, 'sum', {
+  value: function () {
+    return this.reduce((a: number, b: number) => a + b, 0);
+  },
+});
+
 console.log('part1:', part1(input));
 console.log('part2:', part2(input));
+
+function trim(s: string) {
+  return s.trim();
+}
+
+function split(seperator: string, s: string) {
+  return s.split(seperator);
+}
+
+function pipe(...fns: ((a: any) => any)[]) {
+  return (v: any) => fns.reduce((a, f) => f(a), v);
+}
+
+function curry(fn: (...args: any[]) => any) {
+  return function (...args: any[]) {
+    // fn.length is number of arguments
+    if (args.length >= fn.length) {
+      return fn.apply(null, args); // just call fn
+    }
+    // bind a new function with passed arguments
+    return fn.bind(null, ...args);
+  };
+}
